0166b735b2b0faa5e5b5a433d939f82acace742c
mx_steve <mx_steve@163.com>
2021-08-20 11:24:10 +0800

    add documents

--- documents/workers/jobs/scripts/services/demo/ktz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from ktz.api.test import TestView
-
-app_name = 'ktz'
-
-urlpatterns = [
-    url(r'^test/$', TestView.as_view(), name='test'),
-]
--- documents/workers/jobs/scripts/services/demo/ktzIn/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import include
-from ktz.check.check import check
-
-urlpatterns = [
-    url(r'^ktz/',include('ktz.urls')),
-    url(r'isAlive',check.as_view())
-]
