5d7028281288ce3ae6fed2dde1b266fa0a4f3774
mx_steve <mx_steve@163.com>
2021-08-20 11:54:50 +0800

    remove k8s

--- documents/workers/jobs/scripts/k8s/jenkins/new-pipelines/projects/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ProjectName=$1
-if [ -d /root/update/projects/$ProjectName ];then
-  helm uninstall $ProjectName
-  helm install $ProjectName /root/update/projects/${ProjectName}/
-else
-  echo "项目为新项目，请联系管理员手动部署"
-  exit 1
-fi
--- documents/workers/jobs/scripts/k8s/namespaces/scripts/get_rs_yamls.sh
+++
@@ -1,7 +0,0 @@
-ns=$1 # namespace
-rs=$2 # resource type: cm/deployment/svc/ingress/secret
-mkdir $rs
-for cm in `kubectl get $rs -n $ns | awk 'NR!=1{print $1}'`
-do
-  kubectl get $rs $cm -n $ns -o yaml > $rs/$cm.yaml
-done
